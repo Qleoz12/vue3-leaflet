75d12cc57e533ebe2a8e6c0262f8445b4d50ac1d
Beaumont Yun <b.yun@hotmail.com>
2021-07-21 18:00:01 +0800

    Added vue3-leaflet map layer

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,37 +1,107 @@
 <template>
   <div class="mapContainer">
-    <l-map v-model="zoom" v-model:zoom="zoom">
-
+    <l-map @ready="onStart()" ref="mymap" v-model="zoom" v-model:zoom="zoom" :center="center">
+      <l-tile-layer :url="url" :attribution="attribution"> </l-tile-layer>
+      <l-control class="clickControl" :position="position">
+        <p @click="showClick">Control</p>
+      </l-control>
     </l-map>
+    <div class="controller">
+      Controls:
+      <div>
+        <p>Lat:</p>
+        <form action="">
+          <input
+            v-model.number="latitude"
+            type="numbers"
+            placeholder="lattitude"
+          />
+        </form>
+
+        <p>Lng:</p>
+        <form action="">
+          <input
+            v-model.number="longitude"
+            type="numbers"
+            placeholder="longitude"
+          />
+        </form>
+        <br />
+        <button @click="flyToTarget()">Save</button>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import {LMap, LTileLayer, LControlLayers} from "@vue-leaflet/vue-leaflet";
+import {
+  LMap,
+  LTileLayer,
+  LControlLayers,
+  LControl,
+} from "@vue-leaflet/vue-leaflet";
 import "leaflet/dist/leaflet.css";
+import { onMounted } from 'vue';
 
 export default {
-  name: 'Home',
+  name: "Home",
   components: {
     LMap,
     LTileLayer,
     LControlLayers,
+    LControl,
   },
   data() {
     return {
-      zoom: 15,
-    }
+      map: null,
+      zoom: 13,
+      url: "https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png",
+      attribution: `&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>`,
+      center: [51.505, -0.09],
+      position: "topright",
+      latitude: "",
+      longitude: "",
+    };
   },
-  computed: {
-
+  methods: {
+    onStart(){
+      // this.$nextTick(() => {
+      this.map = this.$refs.mymap.mapObject;
+    // });
+    },
+    showClick() {
+      console.log("clicked");
+    },
+    flyToTarget() {
+      console.log("lat " + this.latitude + " and " + "lng " + this.longitude);
+      this.map.flyTo(this.center, 13);
+    },
   },
-}
+};
 </script>
 
 <style scoped>
-  .mapContainer{
-    height: 96vh;
-    width: 100vh;
-  }
+.mapContainer {
+  height: 65vh;
+  width: 100%;
+  z-index: 100;
+  border: 1px solid black;
+}
+.clickControl {
+  background: #fff;
+  padding: 0 0.5rem;
+  border: 1px solid black;
+  border-radius: 10px;
+  cursor: pointer;
+  user-select: none;
+  -moz-user-select: none;
+  -webkit-touch-callout: none;
+  -webkit-user-select: none;
+}
+.controller {
+  /* border: 1px solid black; */
+  height: 33vh;
+  width: 100%;
+}
 </style>
