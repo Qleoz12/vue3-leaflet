9c1b76b9f227d5ac0b82103ece9cf11eb706f4a7
qleoz12 <kintaroloop@gmail.com>
2022-10-12 00:46:53 -0500

    modificaciones
    - separacion de front and backend
    - creacion  de api con mongo

--- src/helpers/number.d.ts
+++
@@ -1,8 +0,0 @@
-declare module 'vue' {
-    interface Number {
-      $Number: typeof Number
-      // Extend Number object with methods to convert between degrees & radians
-        toRadians =()=> function() { return this * Math.PI / 180; };
-        toDegrees =()=> function() { return this * 180 / Math.PI; };
-    }
-  }
